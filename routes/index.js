bb0fc4921aca3df1d5d2264e34afc5f911d86a15
Carol0311 <18710371681@163.com>
2017-07-19 12:31:04 +0800

    test url

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/', require('./welcome'));
 router.get('/login', require('./login'));
 router.get('/user', require('./user'));
+router.get('/recommend', require('./recommend'));
 router.all('/tunnel', require('./tunnel'));
 
 module.exports = router;
