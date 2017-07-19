0da2c91ada4629f4414db7027b44b25ae221595a
Carol0311 <18710371681@163.com>
2017-07-19 09:06:02 +0800

    try to change code

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', require('./welcome'));//
-router.get('/login', require('./login'));
+router.get('/', require('./welcome'));
+//router.get('/login', require('./login'));
 //router.get('/user', require('./user'));
 router.all('/tunnel', require('./tunnel'));
 
